1d4226704cd7acff4279cb84955b9d431338a10d
Alireza Khorami <khoramism@gmail.com>
2021-10-11 12:59:54 +0330

    .gitignore file added. MyUser intitilized.

--- Backend/koji/koji/settings.py
+++ Backend/koji/koji/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Externals
+
+
+
+
+    # Interals 
+    'poll',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +131,15 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest related
+
+# ckeditor related
+
+# Image related
+
+
+# Auth related
+
+
+AUTH_USER_MODEL = 'account.MyUser'
