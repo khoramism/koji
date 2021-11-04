1a4ea7f183262cd5f3c77931e36396e45defedb8
khoramism <khoramism@gmail.com>
2021-11-04 09:16:02 +0330

    Added .env file and config, Added the PSQL settings

--- Backend/koji/koji/settings.py
+++ Backend/koji/koji/settings.py
@@ -15,13 +15,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6_9#=$^a6)q)zh_g7e_t26x5$5qm2#vxm$6uns+)5mvus$%mh-'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -88,12 +93,25 @@ WSGI_APPLICATION = 'koji.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+# PostgreSQL   
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME" : env("DATABASE_NAME"),
+        "USER" : env("DATABASE_USER"),
+        "PASSWORD" : env("DATABASE_PASS"),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
 
 
 # Password validation
