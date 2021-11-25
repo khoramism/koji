22d9281dfa563f4f3489b1398d77bbc7db19d69f
Iman Sedgh <iman.sedgh@yahoo.com>
2021-11-25 18:14:02 +0330

    user teams added to UserDetail Endpoint

--- Backend/koji/api/serializers/UserSerializers.py
+++ Backend/koji/api/serializers/UserSerializers.py
@@ -1,18 +1,21 @@
 from account.models import UserProfile
 from rest_framework import serializers
+from .TeamSerializers import TeamSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    teams = TeamSerializer(many=True)
+
     class Meta:
         model = UserProfile
-        fields = ['email', 'name', 'is_active', 'is_staff']
+        fields = ['email', 'name', 'is_active', 'is_staff', 'teams']
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['email', 'name',"password"]
-    
+        fields = ['email', 'name', "password"]
+
     def create(self, validated_data):
         user = UserProfile(
             email=validated_data["email"],
